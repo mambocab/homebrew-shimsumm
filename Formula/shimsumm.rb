534ac8781047a2443f0f16db1907fa08f46dad5b
Jim Witschey <jim.witschey@gmail.com>
2026-03-18 21:01:30 -0400

    Initial commit: Homebrew tap formula for shimsumm
    Build-from-source formula using head-only install. URL placeholders
    to be updated when the shimsumm repo gets a GitHub remote.

---
+++ Formula/shimsumm.rb
@@ -0,0 +1,16 @@
+class Shimsumm < Formula
+  desc "Transparent output filtering for LLM-managed shells"
+  homepage "https://github.com/OWNER/shimsumm"
+  head "https://github.com/OWNER/shimsumm.git", branch: "main"
+  license "MIT"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args(ldflags: "-s -w"), "./cmd/shimsumm"
+  end
+
+  test do
+    assert_match "Usage:", shell_output("#{bin}/shimsumm", 1)
+  end
+end
